584a973a4dd41fd92fe397fb0c932ec2101e2d4d
ahmedeisa22 <70604321+ahmedeisa22@users.noreply.github.com>
2023-07-15 12:50:54 +0300

    Create pattern.cs

---
+++ 290. Word Pattern/pattern.cs
@@ -0,0 +1,31 @@
+  public  bool WordPattern(string pattern, string s)
+        {
+            var dict = new Dictionary<char, string>();
+            bool res = true;
+
+            var texts = s.Split(" "); // split words by spaces
+
+            if (pattern.Length != texts.Length)
+                return false;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!dict.ContainsKey(pattern[i]))
+                {
+                   
+                    dict[pattern[i]] = texts[i];
+                }
+                else
+                {
+                    if (dict[pattern[i]] != texts[i])
+                        return false;
+                }
+            }
+            // if two values duplicated then two key has same pattern return false
+            bool hasDuplicate = dict.GroupBy(g => g.Value)
+                                  .Any(g => g.Count() > 1);
+            if (hasDuplicate)
+                return false;
+
+            return res;
+        }
